0ee01fe6852e2e9d0f4f5a3f92a0c98540c9d9c4
guillericci <guillermo.nicolas.ricci@gmail.com>
2025-05-27 23:53:19 -0300

    BoundingBox

--- problema1_PDI.py
+++ problema1_PDI.py
@@ -38,19 +38,26 @@ imshow(f, new_fig=False, title="Imagen Original", ticks=True)
 plt.subplot(122, sharex=ax, sharey=ax), imshow(f_blur, new_fig=False, title="Gaussian Blur")
 plt.show(block=False)		 
 
-gcan2 = cv2.Canny(f_blur, threshold1=0.4*255, threshold2=0.5*255)
+gcan2 = cv2.Canny(f_blur, threshold1=0.3*255, threshold2=0.4*255) #0.4-0.5
 
 plt.figure()
 ax = plt.subplot(221)
 imshow(f, new_fig=False, title="Imagen Original")
-plt.subplot(223, sharex=ax, sharey=ax), imshow(gcan2, new_fig=False, title="Canny - U1=40% | U2=50%")
+plt.subplot(223, sharex=ax, sharey=ax), imshow(gcan2, new_fig=False, title="Canny - U1=20% | U2=40%")
 plt.show(block=False)
+
+
 
 # ---- Clausura (Closing) -----------------------
 A = gcan2
 #imshow(A, title="Imagen Original")
 
-B = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))
+B = cv2.getStructuringElement(cv2.MORPH_RECT, (22, 11)) #(25,12)
+
+#kernel_dilatacion = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+#A_dilatado = cv2.dilate(gcan2, kernel_dilatacion)
+
+#Aclau = cv2.morphologyEx(A_dilatado, cv2.MORPH_CLOSE, B)
 Aclau = cv2.morphologyEx(A, cv2.MORPH_CLOSE, B)
 
 plt.figure()
@@ -59,10 +66,11 @@ plt.subplot(122, sharex=ax1, sharey=ax1); imshow(Aclau, new_fig=False, title="Cl
 plt.show(block=False)
 
 # ---- Componentes conectados -----------------------
-connectivity = 4
+connectivity = 8
 num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(Aclau, 
-connectivity, cv2.CV_32S)
+connectivity, cv2.CV_32S)#cv2.CV_32S cv2.CC_STAT_AREA
 
+# ---- Bounding box (codigo profe)
 im_color = cv2.applyColorMap(np.uint8(255/num_labels*labels), cv2.COLORMAP_JET)
 for centroid in centroids:
     cv2.circle(im_color, tuple(np.int32(centroid)), 9, color=(255,255,255), thickness=-1)
@@ -70,4 +78,21 @@ for st in stats:
     cv2.rectangle(im_color,(st[0],st[1]),(st[0]+st[2],st[1]+st[3]),color=(0,255,0),thickness=2)
 imshow(img=im_color, color_img=True)
 
+# ---- Bounding box (corregido y adaptado a  area)
+MIN_AREA = 3000
+im_color = cv2.applyColorMap(np.uint8(255 / num_labels * labels), cv2.COLORMAP_JET)
 
+for centroid in centroids[1:]:  # Omitimos el fondo
+    cv2.circle(im_color, tuple(np.int32(centroid)), 9, color=(255,255,255), thickness=-1)
+
+for i in range(1, num_labels):
+    if stats[i, cv2.CC_STAT_AREA] < MIN_AREA:
+        continue
+    x, y, w, h = stats[i, :4]
+    cv2.rectangle(im_color, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
+
+imshow(img=im_color, color_img=True)
+
+ 
+#AREA CHIP: 178164
+#AREA RESISTENCIAS:
